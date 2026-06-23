0f52df8932ef263120d59f5c811d895f6098c75d
kingofking8118-ctrl <kingofking8118@gmail.com>
2026-06-23 15:53:07 +0330

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.querySelectorAll('.card').forEach(c=>{
-c.addEventListener('mousemove',()=>c.style.transform='translateY(-6px) scale(1.02)');
-c.addEventListener('mouseleave',()=>c.style.transform='');
-});
